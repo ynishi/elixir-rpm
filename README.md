162d243b517f7c19a5e74dfce083816a793ca196
Yutaka Nishimura <ytk.nishimura@gmail.com>
2016-07-23 14:31:01 +0000

    mod README, add build script

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # elixir-rpm
 
 To build Elixir rpm for CentOS.
+
+## Build rpm
+
+1. Prepare Any New CentOS 7 environment
+2. do build.sh
+
+## rpm is available
+
+The rpm made with this repository is available in copr.
+You can use like below.
+```bash
+$ curl -O https://copr.fedorainfracloud.org/coprs/ynishi/elixir/repo/epel-7/ynishi-elixir-epel-7.repo
+$ sudo mv ynishi-elixir-epel-7.repo /etc/yum.repos.d/. 
+
+# suitable version of erlang is in same repo
+$ sudo yum install elixir
+$ iex -v
+```
+And you can use Phoenix.
+```
+$ mix local.hex
+$ mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+$ mix phoenix.new web --no-branch --no-exto
+```
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Simple build script
+#
+
+#install build tools
+sudo yum -y install rpm-build rpmdevtools yum-utils mock
+sudo yum -y groupinstall "Development Tools" 
+sudo yum install -y epel-release
+
+rpmdev-setuptree
+
+cd ~/rpmbuild/SRPMS/ 
+
+# install erlang
+curl -OL http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/e/elixir-1.3.1-1.fc25.src.rpm
+
+rpm -ivh elixir-1.3.1-1.fc25.src.rpm 
+
+sudo yum-builddep -y ~/rpmbuild/SRPMS/erlang-18.3.4.1-1.fc25.src.rpm
+
+rpmbuild -ba ~/rpmbuild/SPECS/erlang-18.3.4.1-1.fc25.src.rpm
+sudo yum localinstall -y ~/rpmbuild/RPMS/*/erlang*
+
+# build elixir rpm
+rpmbuild -ba ~/rpmbuild/SPECS/elixir.spec
