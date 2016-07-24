26db5432f38c7f4f2fbb1dcf32da81c03ca6b0ba
Yutaka Nishimura <ytk.nishimura@gmail.com>
2016-07-24 05:17:22 +0000

    add rpm script and fix build.sh for el6

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # Simple build script
 #
 
+BASEDIR="$(cd $(dirname $0) && pwd)"
+
 #install build tools
 sudo yum -y install rpm-build rpmdevtools yum-utils mock
 sudo yum -y groupinstall "Development Tools" 
@@ -18,23 +20,23 @@ curl -OL http://dl.fedoraproject.org/pub/epel/6/SRPMS/erlang-R14B-04.3.el6.src.r
 sudo yum-builddep -y --nogpgcheck ~/rpmbuild/SRPMS/erlang-R14B-04.3.el6.src.rpm
 rpm -ivh erlang-R14B-04.3.el6.src.rpm
 
-cp SOURCES/*.patch ~/rpmbuild/SOURCES
-#cd ~/rpmbuild/SOURCES
+cp ${BASEDIR}/SOURCES/* ~/rpmbuild/SOURCES
+
+cd ~/rpmbuild/SOURCES
+
 #git clone http://pkgs.fedoraproject.org/git/rpms/erlang.git
 #cd erlang
 #cp otp-*.patch ~/rpmbuild/SOURCES
 
 curl -OL https://github.com/erlang/otp/archive/OTP-18.3.4.1/otp-OTP-18.3.4.1.tar.gz
 
-cp SPECS/*.spec ~/rpmbuild/SPECS/
+cp ${BASEDIR}/SPECS/*.spec ~/rpmbuild/SPECS/
 
 sudo yum -y install erlang fop libxslt wxGTK3-devel
-
 rpmbuild -ba ~/rpmbuild/SPECS/erlang.spec
 
 sudo yum localinstall -y ~/rpmbuild/RPMS/*/erlang*
 
 # build elixir rpm
 curl -OL https://github.com/elixir-lang/elixir/archive/v1.3.1/elixir-1.3.1.tar.gz
-mv elixir-1.3.1.tar.gz ~/rpmbuild/SOURCES
 rpmbuild -ba ~/rpmbuild/SPECS/elixir.spec
